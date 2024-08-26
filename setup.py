80c027cc52b531ca0cc031fee7d8f645146e71f2
Antoine Debouchage <antoine311200@gmail.com>
2024-08-26 18:24:08 +0900

    Add setup of package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="tn-gradient",
+    version="1.0",
+    author="Antoine Debouchage",
+    author_email="antoine311200@gmail.com",
+    packages=["tn_gradient"],
+)
