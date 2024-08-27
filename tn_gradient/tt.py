47ac99971d349c7f4c23fbdb9a15666aec7da12e
Antoine Debouchage <antoine311200@gmail.com>
2024-08-27 16:47:10 +0900

    Implement square root and inverse square root iterative algorithm

--- tn_gradient/tt.py
+++ tn_gradient/tt.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 
-from math import floor, log
+from math import floor, log, sqrt
 
 from opt_einsum import contract
 from scipy import linalg
@@ -13,13 +13,14 @@ class TensorTrain:
         self.ranks = ranks
         self.in_shape = in_shape
         self.out_shape = out_shape
-
         
-        self.cores = [torch.empty((ranks[i], in_shape[i], out_shape[i], ranks[i+1])) for i in range(self.order)]
-        if device:
-            self.to(device)
-        else:
-            self.device = self.cores[0].device
+        self.cores = [None for _ in range(self.order)]
+        self.device = device
+        #torch.empty((ranks[i], in_shape[i], out_shape[i], ranks[i+1])) for i in range(self.order)]
+        # if device:
+            # self.to(device)
+        # else:
+        #     self.device = self.cores[0].device
 
     @staticmethod
     def from_tensor(tensor: torch.Tensor, ranks: list):
@@ -44,14 +45,14 @@ class TensorTrain:
         return tt
     
     @staticmethod
-    def zeros(ranks, in_shape, out_shape, device):
+    def zeros(ranks, in_shape, out_shape, device="cpu"):
         tt = TensorTrain(ranks, in_shape, out_shape)
         tt.cores = [torch.zeros((ranks[i], in_shape[i], out_shape[i], ranks[i+1])) for i in range(tt.order)]
         tt.to(device)
         return tt
     
     @staticmethod
-    def ones(ranks, in_shape, out_shape, device):
+    def ones(ranks, in_shape, out_shape, device="cpu"):
         tt = TensorTrain(ranks, in_shape, out_shape)
         tt.cores = [torch.ones((ranks[i], in_shape[i], out_shape[i], ranks[i+1])) for i in range(tt.order)]
         tt.to(device)
@@ -71,8 +72,7 @@ class TensorTrain:
     
     def clone(self):
         tt = TensorTrain(self.ranks.copy(), self.in_shape, self.out_shape)
-        tt.cores = [core.clone() for core in self.cores]
-        tt.to(self.device)
+        tt.cores = self.cores.copy()
         return tt
 
     def decompose(self, tensor: torch.Tensor):
@@ -199,7 +199,38 @@ class TensorTrain:
             struct.append((f"rank2_{i}", f"in_{i}", f"out_{i}", f"rank2_{i+1}"))
         return float(contract(*struct).squeeze())
 
-    def sqrt(self):
+    def sqrtinv(self, threshold=1e-8):
+        """Compute the element-wise reciprocal of the square root of the tensor train
+        using the Newton method to the equation (1/x^2 - y = 0)"""
+
+        # Find the maximum value of the tensor train
+        # Then, compute the number of bits to shift the tensor train to the right
+        # This allows the square root algorithm to converge
+        max_value = float(max([core.abs().max() for core in self.cores]))
+        k = floor(log(max_value) / log(4))
+        c, revc = (1/(4**k)), (1/2**(k-1))
+
+        # Scale the tensor train by 1/4^k
+        A = c * self.clone()
+
+        max_iter = 10
+        while max_iter > 0:
+            B = -1/2 * (self * (A * A)).add_(-3)
+            B = B.round(self.ranks)
+            C = A * B
+            C = C.round(self.ranks)
+
+            if threshold:
+                if abs((C - A).norm()) < threshold:
+                    return revc * C
+            A = C
+            max_iter -= 1
+            
+        # Scale back the tensor train by 2^(k-1) to account for the initial scaling
+        A = revc * A
+        return A
+
+    def sqrt(self, threshold=1e-3):
         """Compute the element-wise square root of the tensor train
         using an iterative method.
         This is a SLOW method and should be used with caution.
@@ -217,7 +248,7 @@ class TensorTrain:
 
         ranks = A.ranks.copy()
         max_iter = 10
-        while max_iter > 0 and (A - C).norm() > 1e-4:
+        while max_iter > 0 and (A - C).norm() > threshold:
             B = A - 1/2 * (A * C)
             B = B.round(ranks)
             D = 1/4 * (C * C).round(ranks) * (C.add_(-3))
@@ -228,8 +259,6 @@ class TensorTrain:
         # Scale back the tensor train by 2^k to account for the initial scaling
         A = 2**k * A
         return A
-
-
 
     def add_(self, constant):
         """Add a constant to the tensor train, inplace.
@@ -246,7 +275,29 @@ class TensorTrain:
         """
         n_inner_params = torch.prod(torch.tensor(self.ranks))
         subconstant = constant / n_inner_params
-        return self + subconstant * TensorTrain.ones(self.ranks, self.in_shape, self.out_shape, device=self.device)
+        is_neg = subconstant < 0
+        subconstant = abs(subconstant) ** (1 / self.order)
+
+        cores = []
+        for i in range(self.order):
+            leftmost = i == 0
+            rightmost = i == self.order - 1
+
+            left_matrix = self.cores[i]
+            right_matrix = torch.full_like(left_matrix, ((-1 if is_neg else 1) * subconstant))
+
+            if leftmost:
+                new_core = torch.cat((left_matrix, right_matrix), dim=-1)
+            elif rightmost:
+                new_core = torch.cat((left_matrix.transpose(-2, -1), right_matrix.transpose(-2, -1)), dim=0).transpose(-2, -1)
+            else:
+                left_matrix_padded = F.pad(left_matrix, (0, self.ranks[i+1], 0, 0))
+                right_matrix_padded = F.pad(right_matrix, (self.ranks[i], 0, 0, 0))
+                new_core = torch.cat([left_matrix_padded, right_matrix_padded], dim=0)
+
+            cores.append(new_core)
+        return TensorTrain.from_cores(cores)
+        # return self + subconstant * TensorTrain.ones(self.ranks, self.in_shape, self.out_shape, device=self.device)
 
     def __add__(self, other):
         """Add two tensor trains element-wise.
@@ -292,7 +343,7 @@ class TensorTrain:
 
     def __sub__(self, other):
         """Subtract two tensor trains element-wise."""
-        return self + (-1) * other      
+        return self + (-1) * other   
 
     def __rmul__(self, constant):
         """Multiply a tensor train by a constant.
@@ -345,7 +396,23 @@ class TensorTrain:
 
             cores.append(new_core)
         return TensorTrain.from_cores(cores)
-
+    
+    def reciprocal(self):
+        """Divide two tensor trains element-wise."""
+        cores = []
+        for i in range(self.order):
+            new_core = torch.zeros_like(self.cores[i])
+            for inp in range(self.in_shape[i]):
+                for out in range(self.out_shape[i]):
+                    if i == 0:
+                        new_core[:, inp, out, :] = self.cores[i][:, inp, out, :]
+                    elif i == self.order - 1:
+                        new_core[:, inp, out, :] = self.cores[i][:, inp, out, :]
+                    else:
+                        new_core[:, inp, out, :] = torch.linalg.inv(self.cores[i][:, inp, out, :])
+            cores.append(new_core)
+        return TensorTrain.from_cores(cores)
+       
     def left_matrix(self, index):
         """Left matrizification of the core at index. C(rk-1, ik, jk, rk) -> C(rk-1 * ik * jk, rk)"""
         return self.cores[index].reshape((self.ranks[index] * self.in_shape[index] * self.out_shape[index], -1))
