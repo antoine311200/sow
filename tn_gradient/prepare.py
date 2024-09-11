aa68e7498ce32deb83fa8893962a20a3b4a3faad
Antoine Debouchage <antoine311200@gmail.com>
2024-09-11 18:13:43 +0900

    Formatting the code

--- tn_gradient/prepare.py
+++ tn_gradient/prepare.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 
 from tn_gradient.layer.sow import SoWLinear, SoWArgs
 
-def prepare_sow(model, target_modules, decompose: bool = True, args: SoWArgs = SoWArgs()):
+
+def prepare_sow(
+    model, target_modules, decompose: bool = True, args: SoWArgs = SoWArgs()
+):
     """Code for preparing the model for the Sum-of-Weights decomposition.
 
     Given a model and a list of target modules, this function will replace the target modules with the Sum-of-Weights decomposed layers.
     For pre-traning, the decomposition is not necessary as we can reduce the memory overhead with an empty accumulated weights until the
     first accumulation step. This is done by setting the `decompose` argument to False.
     For fine-tuning, the decomposition is necessary to keep the original weights of the model.
-    
+
     This decomposition is done as follows:
     1. The weight matrix of the target module is decomposed into two matrices, Q and R, using the QR decomposition.
     2. The Q matrix is split into two matrices: the major part and the minor part.
@@ -31,7 +34,7 @@ def prepare_sow(model, target_modules, decompose: bool = True, args: SoWArgs = S
     """
     layers_to_replace = {}
     for name, module in model.named_modules():
-        if isinstance(module, nn.Linear) and name.split('.')[-1] in target_modules:
+        if isinstance(module, nn.Linear) and name.split(".")[-1] in target_modules:
             layers_to_replace[name] = module
 
     for name, module in layers_to_replace.items():
@@ -54,10 +57,17 @@ def prepare_sow(model, target_modules, decompose: bool = True, args: SoWArgs = S
         )
 
         if decompose:
+            keep_rank = args.rank * args.n_iter
             Q, R = torch.linalg.qr(module.weight.data.T)
-            Q_major, Q_minor = Q[:, :-args.n_iter*args.rank], Q[:, -args.n_iter*args.rank:]
-            R_major, R_minor = R[:-args.n_iter*args.rank, :], R[-args.n_iter*args.rank:, :]
-            
+            Q_major, Q_minor = (
+                Q[:, :-keep_rank],
+                Q[:, -keep_rank:],
+            )
+            R_major, R_minor = (
+                R[:-keep_rank, :],
+                R[-keep_rank:, :],
+            )
+
             W = Q_major @ R_major
             A = torch.split(Q_minor, args.rank, dim=1)
             B = torch.split(R_minor, args.rank, dim=0)
@@ -74,11 +84,11 @@ def prepare_sow(model, target_modules, decompose: bool = True, args: SoWArgs = S
             new_layer.downscale_weights.to(weight_device).type(weight_type)
             new_layer.upscale_weights.to(weight_device).type(weight_type)
 
-        if '.' in name:
-            parent_name, child_name = name.rsplit('.', 1)
+        if "." in name:
+            parent_name, child_name = name.rsplit(".", 1)
             parent_module = dict(model.named_modules())[parent_name]
             setattr(parent_module, child_name, new_layer)
         else:
             setattr(model, name, new_linear)
-    
+
     return model
