e51cafbd0f0fa13f6ae4182435fe4ed67ec43793
Antoine Debouchage <antoine311200@gmail.com>
2024-09-30 14:21:38 +0900

    Misc changes from a few weeks

--- scripts/launch_glue.py
+++ scripts/launch_glue.py
@@ -3,17 +3,18 @@ import os
 
 # wandb.login()
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 os.system("python run_glue.py \
     --model_name_or_path roberta-base \
     --task_name mrpc \
-    --enable_sow \
-    --rank 10 \
-    --n_iter 20 \
     --max_length 512 \
     --seed=1234 \
     --per_device_train_batch_size 16 \
     --learning_rate 3e-5 \
     --num_train_epochs 30 \
+    --enable_sow \
+    --n_iter 10 \
+    --rank 10 \
+    --accumulation_steps 500 \
     --output_dir results/ft/roberta_base/mrpc")
